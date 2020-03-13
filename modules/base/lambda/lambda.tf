022b3e22b0542a9e55bd128aa4c507657bfaaae0
John McCormick <john.mccormick@investec.co.uk>
2020-03-13 16:46:58 +0000

    change

--- modules/base/lambda/lambda.tf
+++ modules/base/lambda/lambda.tf
@@ -84,7 +84,8 @@ resource "aws_cloudwatch_metric_alarm" "test_lambda" {
 }
 
 resource "aws_sns_topic_subscription" "pagerduty" {
-  endpoint               = "https://events.pagerduty.com/integration/09f194d2af3d47b392f907d698be37b0/enqueue"
+  #endpoint               = "https://events.pagerduty.com/integration/09f194d2af3d47b392f907d698be37b0/enqueue"
+  endpoint               = "https://events.pagerduty.com/integration/771d5066657c457ab89339757e5a1347/enqueue"
   endpoint_auto_confirms = true
   protocol               = "https"
   topic_arn              = aws_sns_topic.alarms.arn
